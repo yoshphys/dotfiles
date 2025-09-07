dd49e048f6afb0b4a150c8c0fb858d378101924a
yoshphys <nakada.yosh@gmail.com>
2025-09-07 22:02:03 +0900

    add some packages used in homebrew

--- config/nix/hosts/debby/packages.nix
+++ config/nix/hosts/debby/packages.nix
@@ -1,29 +1,53 @@
 { pkgs, ... }:
 with pkgs; [
+  # terminal utility ###############################
   sheldon # zsh plugin manager
   starship # prompt decoration
-  eza # rich ls
+  eza # richer ls
+  tree
+  fd
+  fzf
+  ripgrep
+  unar # richer unzip
+  peco # richer `command | grep`
+  tmux
 
-  deno
-  peco
+  # system #########################################
+  bottom # richer top
+  mosh
+  mutagen
+
+  # git ############################################
   gh # github cli
   ghq
   lazygit
-  fzf
-  unar # rich unzip
+
+  # programming ####################################
+  deno
+  # rust # found no package
+  # lua
+
+  # productivity ###################################
+  typst
+  marp-cli
+  pandoc
+
+  # science/technology #############################
+  root
+  gnuplot
 
   # AI #############################################
   gemini-cli
+
+  # editor #########################################
+  vim-startuptime
+  neovim
 
   # LSP/formatter ##################################
   tinymist
   basedpyright
   lua-language-server
   copilot-language-server
-
-  # editor #########################################
-  vim-startuptime
-  neovim
 
   # brewCasks ######################################
   brewCasks.inkscape
