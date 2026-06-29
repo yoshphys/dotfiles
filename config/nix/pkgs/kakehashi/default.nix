b5a0ec711ece1f6afd94d4a324983b0345dc1410
yoshphys <nakada.yosh@gmail.com>
2026-06-29 10:00:33 +0900

    kakehashi binary was directory accuired from github releases

--- config/nix/pkgs/kakehashi/default.nix
+++ config/nix/pkgs/kakehashi/default.nix
@@ -1,27 +1,28 @@
-{
-  lib,
-  fetchFromGitHub,
-  rustPlatform,
-}:
+{ lib, stdenv, fetchurl }:
 let
-  pname = "kakehashi";
-  version = "0.6.0";
+  version = "0.7.0";
 in
-rustPlatform.buildRustPackage {
-  inherit pname version;
-  src = fetchFromGitHub {
-    owner = "atusy";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-DxTEKrG1eWtjFJYlwuSsLkeowjuATxpRABQgExb4bW4=";
-  };
+stdenv.mkDerivation {
+  pname = "kakehashi";
+  inherit version;
 
-  cargoLock.lockFile = ./Cargo.lock;
+  src = fetchurl {
+    url = "https://github.com/atusy/kakehashi/releases/download/v${version}/kakehashi-v${version}-aarch64-apple-darwin.tar.gz";
+    hash = "sha256-0uQk+PcVEuea2xUz/Xwb0u+7O7BjcAfmCCOK08kLlY8=";
+  };
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    tar -xf $src
+    install -Dm755 kakehashi $out/bin/kakehashi
+  '';
 
   meta = {
     description = "Tree-sitter based language server bridge";
     homepage = "https://github.com/atusy/kakehashi";
     license = lib.licenses.mit;
-    platforms = lib.platforms.all;
+    platforms = [ "aarch64-darwin" "x86_64-darwin" ];
+    mainProgram = "kakehashi";
   };
 }
