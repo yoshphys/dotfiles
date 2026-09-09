435277d69763f90c72146e0da7794316b1bb4025
yoshphys <nakada.yosh@gmail.com>
2026-09-09 11:00:21 +0900

    replace from Pkg.Apps.add to Pkg.Apps.update for julia coding tools

--- config/nix/home-manager/programs/julia.nix
+++ config/nix/home-manager/programs/julia.nix
@@ -8,11 +8,26 @@
 
   home.shellAliases.julia = "julia --banner=no";
 
-  home.activation.installJETLS = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.installJuliaApps = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD ${pkgs.julia-bin}/bin/julia -q << 'EOF'
-    using Pkg
+    using Pkg, TOML
+
+    function app_installed(name::AbstractString)
+        manifest = Pkg.Apps.app_manifest_file()
+        isfile(manifest) || return false
+        return haskey(get(TOML.parsefile(manifest), "deps", Dict{String, Any}()), name)
+    end
+
+    # `Pkg.Apps.update` errors out when the package is not in the app manifest,
+    # so fall back to `Pkg.Apps.add` for the initial installation.
+    function ensure_app(name::AbstractString)
+        app_installed(name) ? Pkg.Apps.update(name) : Pkg.Apps.add(name)
+    end
+
+    # Keep `add` here: `update` would reuse the url/rev recorded in the app
+    # manifest and silently ignore the ones declared below.
     Pkg.Apps.add(; url="https://github.com/aviatesk/JETLS.jl", rev="release")
-    Pkg.Apps.add("Runic")
+    ensure_app("Runic")
     EOF
   '';
 }
